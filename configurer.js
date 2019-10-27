a7bd63a56f1f8f1689e02416abd072580418e375
MrAugu <39545629+MrAugu@users.noreply.github.com>
2019-10-27 08:59:39 +0200

    Configurer Polishing

--- configurer.js
+++ configurer.js
@@ -1,17 +1,47 @@
 const rls = require("readline-sync");
 const fs = require("fs");
 
+try {
+  require("./configuration.json");
+} catch (e) {
+  console.log("Configuration file not found.");
+  console.log("Creating a configuration file...");
+  const defaults = {
+    "_token": "TOKEN",
+    "_prefix": "-",
+    "_owner": "ID",
+    "_admins": [],
+    "_mongoUri": "MONGODB_URL",
+    "_mainGuild": "ID",
+    "_staffGuild": "ID",
+    "_parent": "ID",
+    "_supportRole": "ID",
+    "_redTickEmoji": "❌",
+    "_greenTickEmoji": "✅"
+  };
+  console.log("Inserting defaults...");
+  
+  fs.writeFileSync("configuration.json", JSON.stringify(defaults, null, 4));
+
+  console.log("Created a new configuration file.");
+  console.log("Loading configurer...");
+}
+
 var defs = fs.readFileSync("configuration.json", "utf8");
 defs = JSON.parse(defs);
 
-var token = rls.question(`Enter your bot's token: (${defs._token}) `);
-var prefix = rls.question(`Enter your default prefix: (${defs._prefix}) `);
-var owner = rls.question(`Enter your discord id: (${defs._owner}) `);
-var mongoURL = rls.question(`Enter your mongo url: (${defs._mongoUri}) `);
-var mainGuild = rls.question(`Enter your main guild id: (${defs._mainGuild}) `);
-var staffGuild = rls.question(`Enter your staff guild id [Can be same as main guild.]: (${defs._staffGuild}) `);
-var parent = rls.question(`Enter category id you want ticket channels to be created in [Must be inside \`staffGuild\`]: (${defs._parent}) `);
-var supportRole = rls.question(`Enter role id of the 'Support Team' role [Must be inside \`staffGuild\`]: (${defs._supportRole}) `);
+console.log("");
+console.log("!!! Enter the values you want, the values in the round brackets are the default ones, if you want any to remain same just hit enter when you are asked to enter that value and the one in round brackets will be not changed. If is the first run, all settings must be changed to valid ones.");
+console.log("");
+
+var token = rls.question(`- Enter your bot's token: (${defs._token}) `);
+var prefix = rls.question(`- Enter your default prefix: (${defs._prefix}) `);
+var owner = rls.question(`- Enter your discord id: (${defs._owner}) `);
+var mongoURL = rls.question(`- Enter your mongo url: (${defs._mongoUri}) `);
+var mainGuild = rls.question(`- Enter your main guild id: (${defs._mainGuild}) `);
+var staffGuild = rls.question(`- Enter your staff guild id [Can be same as main guild.]: (${defs._staffGuild}) `);
+var parent = rls.question(`- Enter category id you want ticket channels to be created in [Must be inside \`staffGuild\`]: (${defs._parent}) `);
+var supportRole = rls.question(`- Enter role id of the 'Support Team' role [Must be inside \`staffGuild\`]: (${defs._supportRole}) `);
 
 if (token.length < 1) token = defs._token;
 if (prefix.length < 1) prefix = defs._prefix;
