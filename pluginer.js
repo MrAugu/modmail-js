f58ec74d78f86a2d1418b34ea45a699d4abc56e9
MrAugu <mraugu@yahoo.com>
2019-10-27 14:14:05 +0200

    Fully Working Plugin Downloader

--- pluginer.js
+++ pluginer.js
@@ -34,7 +34,7 @@ async function main () {
       json.tree = json.tree.filter(i => i.path.includes("."));
 
       await fs.mkdir(`pluins/${registry[input].name}`, { recursive: true }, (err) => {
-        if (err) console.log(err);
+        if (err) err;
       });
 
       var folders = json.tree.filter(i => i.path.includes("/"));
@@ -43,10 +43,9 @@ async function main () {
       for (var x = 0; x < folders.length; x++) {
         var fds = folders[x].split("/");
         fds.pop();
-        console.log(fds);
 
         await fs.mkdir(`plugins/${registry[input].name}/` + fds.join("/"), { recursive: true }, (err) => {
-          if (err) console.log(err);
+          if (err) throw err;
         });
       }
 
@@ -61,6 +60,7 @@ async function main () {
           if (e) console.log(e);
         });
       }
+      console.log("\nPlugin downloaded!");
     } else {
       var [user, repo] = input.split("/");
       if (user && repo) {
@@ -75,8 +75,6 @@ async function main () {
           tree = "master";
         }
 
-        console.log(user, repository, tree);
-
         if (user && repository && tree) {
           console.log(`Downloading ${user}/${repository}.`);
 
@@ -84,9 +82,13 @@ async function main () {
             headers: { "User-Agent": "mod-mail" }
           });
 
-          // Check if is a valid repository.
-
           const json = await body.json();
+
+          if (json.message && json.message === "Not Found") {
+            console.log("\nRepository does not exist on GitHub.\n");
+            process.exit(1);
+          }
+
           if (!json.tree) {
             console.log("!!!");
             console.log("");
@@ -96,27 +98,69 @@ async function main () {
             process.exit(1);
           }
 
-          const fileMap = json.tree.map(i => i.name);
-          if (!fileMap.includes("main.js") && !fileMap.includes("meta.json")) return console.log("\nSpecified repository is NOT a plugin.\n");
-          console.log(fileMap);
+          const fileMap = json.tree.map(i => i.path);
+          if (!fileMap.includes("main.js") || !fileMap.includes("meta.json")) {
+            console.log("Repository not a valid modmail plugin.");
+            process.exit(1);
+          }
+
+          var meta = await fetch(`https://raw.githubusercontent.com/${user}/${repository}/${tree}/meta.json`, {
+            headers: { "User-Agent": "MrAugu" }
+          });
+
+          meta = await meta.text();
+
+          try {
+            meta = JSON.parse(meta)
+          } catch (e) {
+            console.log("Not plugin metadata could not be parsed.");
+            process.exit(1);
+          }
+
+          if (!meta.name) {
+            console.log("Name does not have a plugin, name could not be determined.");
+            process.exit(1);
+          }
+
+          const metaName = `${meta.name}`.toLowerCase().replace(/\s/g, "-");
+
+          json.tree = json.tree.filter(i => i.path.includes("."));
+
+          await fs.mkdir(`pluins/${metaName}`, { recursive: true }, (err) => {
+            if (err) throw err;
+          });
+
+          var folders = json.tree.filter(i => i.path.includes("/"));
+          folders = folders.map(f => f.path);
+
+          for (var x = 0; x < folders.length; x++) {
+            var fds = folders[x].split("/");
+            fds.pop();
+
+            await fs.mkdir(`plugins/${metaName}/` + fds.join("/"), { recursive: true }, (err) => {
+              if (err) throw err;
+            });
+          }
+
           for (var i = 0; i < json.tree.length; i++) {
-            const rawFile = await fetch(`https://api.github.com/repos/${user}/${repository}/contents/${json.tree[i].path}`, {
+            const rawFile = await fetch(`https://raw.githubusercontent.com/${user}/${repository}/${tree}/${json.tree[i].path}`, {
               headers: { "User-Agent": "MrAugu" }
             });
 
-            var file = await rawFile.json();
-            var contents;
-            if (file.contents) contents = Buffer.from(file.content, "base64").toString("ascii");
-            if (!file.contents) contents = await(await (fetch(`${file.download_url}`, {
-              headers: { "User-Agent": "MrAugu" }
-            })).text());
+            var file = await rawFile.text();
 
-            await fs.appendFile(`pluins/${registry[input].name}/` + json.tree[i].path, `${contents}`, (e) => {});
+            await fs.appendFile(`plugins/${metaName}/` + json.tree[i].path, `${file}`, (e) => {
+              if (e) throw e;
+            });
           }
+
+          console.log("\nPlugin downloaded!");
+        } else {
+          return console.log("\nInvalid repository.\n");
         }
+      } else {
+        return console.log("\nInvalid repository.\n");
       }
-      return console.log("\nInvalid repository.\n");
-      console.log(user, repository, tree);
     }
   }
 }
